ae05ca9b8afda33168c2e8064feb1c6de8069e1d
HsHs-dev <hassanswritings@outlook.com>
2024-09-29 11:27:16 +0200

    Adding Lecture05: Machine-language I: Basics.

---
+++ Lectures/Lecture05/swap.c
@@ -0,0 +1,6 @@
+void swap(long *xp, long *yp) {
+    long t0 = *xp;
+    long t1 = *yp;
+    *xp = t1;
+    *yp = t0;
+}
---
+++ Lectures/Lecture05/swap.s
@@ -0,0 +1,34 @@
+	.file	"swap.c"
+	.text
+	.globl	swap
+	.type	swap, @function
+swap:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	movq	(%rdi), %rax
+	movq	(%rsi), %rdx
+	movq	%rdx, (%rdi)
+	movq	%rax, (%rsi)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	swap, .-swap
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
